e0b44339353ad39f8f4111ccfd4d08f1428f5f6a
sidsoeharto <msidiq.15@gmail.com>
2020-11-27 00:26:30 +0700

    added google auth

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,6 +2,8 @@ const bcrypt = require('bcryptjs')
 
 const { User } = require("../models/index")
 const { generateToken } = require('../helpers/generateToken')
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client(GOOGLE_CLIENT_ID);
 
 class Controller {
 
@@ -57,7 +59,39 @@ class Controller {
   }
 
   static googleLogin(req, res, next) {
-    res.send('google-login')
+    let payload
+    //nama token google di jquery
+    const token = req.body.googleToken
+    client.verifyIdToken({
+      idToken: token,
+      audience: process.env.GOOGLE_CLIENT_ID
+    })
+    .then(ticket => {
+      payload = ticket.getPayload()
+      return User.findOne({
+        where: {
+          email: payload.email
+        }
+      })
+    })
+    .then(user => {
+      if(user){
+        return user
+      } else {
+        return User.create({
+          name: payload.name,
+          email: payload.email,
+          password: process.env.GOOGLE_PASSWORD
+        })
+      }
+    })
+    .then(user => {
+      const access_token = generateToken(user)
+      res.status(200).json({access_token})
+    })
+    .catch(err => {
+      next(err)
+    })
   }
 
 
